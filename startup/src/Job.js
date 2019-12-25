a7cb549f3cb804d5c6a4300227f7b6cff3970669
adib surani <adibsur@gmail.com>
2019-12-25 22:44:26 +0800

    1. Initial commit

---
+++ startup/src/Job.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export default function Job({job}) {
+    return (
+        <div className={'job'}>
+            {job.title}
+            {job.company}
+        </div>
+    )
+}
